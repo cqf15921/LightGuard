78224fb318750f019207785ea67f8539382db9a4
CQF15921 <tackettnacole7128@gmail.com>
2026-03-30 22:38:59 +0800

    更新

--- utils/dataset.py
+++ utils/dataset.py
@@ -47,7 +47,7 @@ class TrafficDataset(Dataset):
         return len(self.unique_labels)
 
 
-# 打印 USTC-TFC2016 的参考类别映射
+# 打印 ustc_tfc2016 的参考类别映射
 def get_ustc_mapping():
     return [
         "BitTorrent", "FaceTime", "FTP", "Gmail", "Mysql",
